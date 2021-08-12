c9b2b80848d702a8d802f8889f0edb605e9b3af0
Shuvo Nasir <shuvonasir028@gmail.com>
2021-08-12 09:55:49 +0600

    create dictionary from birthdays.csv

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-##################### Extra Hard Starting Project ######################
+from smtplib import *
+from datetime import *
+from random import *
+import pandas as pd
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
+df_birthdays = pd.read_csv("birthdays.csv")
+birthday_dict = df_birthdays.to_dict(orient="records")
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
